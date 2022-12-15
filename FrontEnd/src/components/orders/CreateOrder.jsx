245f60334f6c54504e47159089b3f66af3b8debe
bladwing <timoti.ponarenko@gmail.com>
2022-12-16 02:20:15 +0400

    add in create order inputs

--- FrontEnd/src/components/orders/CreateOrder.jsx
+++ FrontEnd/src/components/orders/CreateOrder.jsx
@@ -1,49 +1,69 @@
 import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import "./orders.scss";
 
 export default function Createorder() {
   const form = useRef(null);
 
   const [error, setError] = useState(false);
+  const [piecesView, setPiecesView] = useState("none");
+
+  const PiecesTemp = (e) => {
+    setPiecesView(e.target.value);
+    console.log(piecesView);
+  };
+
   return (
     <div>
       <div id="message" className="message">
         {error}
       </div>
 
-      <form ref={form} className="company_form">
+      <form ref={form} className="order_form">
         <table cellSpacing="10" className="mainTable">
           <tbody>
             <tr>
               <th>
-                <label>Company: </label>
+                <label>Order: </label>
               </th>
-              <td></td>
+              <td>
+                <input type="text" placeholder="Order Number" />
+              </td>
             </tr>
 
             <tr>
               <th>
+                <label>Company: </label>
+              </th>
+              <td>
+                <select>
+                  <option>Choose Company</option>
+                  <option></option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <th>
                 <label>Product: </label>
               </th>
-              <td></td>
+              <td>
+                <select onChange={PiecesTemp}>
+                  <option value="none">Choose product</option>
+                  <option value={1}></option>
+                </select>
+              </td>
+              <td>
+                  <input type="text" placeholder="pieces" />
+                </td>
             </tr>
-            <tr>
-              <th>
-                <label>Phone: </label>
-              </th>
-              <td></td>
-            </tr>
-            <tr>
-              <th>
-                <label>Address: </label>
-              </th>
-              <td></td>
-            </tr>
+
+            
+      
           </tbody>
         </table>
         <div className="addPageButton">
           <button className="btn btn-primary btn-sm m-2">Save</button>
-          <Link to="/companies" className=" btn btn-danger btn-sm m-2">
+          <Link to="/orders" className=" btn btn-danger btn-sm m-2">
             Cancel
           </Link>
         </div>
