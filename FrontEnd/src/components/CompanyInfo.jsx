c95809972f88c1db1196e1df9761ce95c552ad3a
bladwing <timoti.ponarenko@gmail.com>
2022-12-27 02:12:36 +0400

    create CompanyInfo showing function

--- FrontEnd/src/components/CompanyInfo.jsx
+++ FrontEnd/src/components/CompanyInfo.jsx
@@ -3,33 +3,50 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Api } from "../utils/api";
 
+const CompanyInfo = () => {
+  const [info, setInfo] = useState([]);
+  const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [account, setAccount] = useState("");
+  const [address, setAddress] = useState("");
+  const [telephone, setTlephone] = useState("");
 
-
-
-const  CompanyInfo = () => {
-
-const [info, setInfo] = useState([]);
+  useEffect(() => {
+    setTimeout(() => {
+      return getCompanyInfo();
+    }, 1000);
+  }, []);
 
   const getCompanyInfo = async () => {
-    const response = await axios.get(Api + "/companies");
-    setLoading(false);
+    const response = await axios.get(Api + "/info");
     setInfo(response.data);
   };
 
-return (
-  <table
-  cellSpacing={10}
-  style={{
-    margin: "2% auto",
-    textAlign: "left",
-  }}
->
+  const updateProduct = async (e) => {
+    e.preventDefault();
+    await axios.patch(Api + `/info`, {
+      name: name,
+      code: code,
+      account: account,
+      address: address,
+      telephone: telephone,
+    });
+    navigate("/");
+  };
 
-  </table>
-)
-
-
-
-  
-}
+  return (
+    <form action="" onSubmit={updateProduct}>
+      <table
+        cellSpacing={10}
+        style={{
+          margin: "2% auto",
+          textAlign: "left",
+        }}
+      >
+        <thead key={info.id}>{info.name}</thead>
+      </table>
+      <button>Update</button>
+    </form>
+  );
+};
 export default CompanyInfo;
